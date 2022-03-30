de2b654fc977f4f245d7a1bd81169a325f2602b7
lukisp2 <lukaszmarchelelk@gmail.com>
2022-03-30 18:04:40 +0200

    Initial commit

---
+++ src/com/company/RentalDto.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class RentalDto {
+
+    private User user;
+    private boolean isRented;
+
+    public RentalDto(final User user, final boolean isRented) {
+        this.user = user;
+        this.isRented = isRented;
+    }
+
+    public User getUser() { return user; }
+
+    public boolean isRented() { return isRented; }
+
+}
